ba7341fea4726fa8736668ab46b0a39a8505031a
Sofia <sfasull1@binghamton.edu>
2023-12-11 06:50:30 -0500

    student page styling

--- js/students.js
+++ js/students.js
@@ -21,3 +21,25 @@ function ondataLoad(data) {
 }
 
 grabStudentOpportunitiesData(ondataLoad);
+
+
+const dataListEl = document.querySelector('#data-list'); 
+const dataTitleEl = document.querySelector('.election-info-title')
+
+//show most recent data as "last updated" and "earliest data from" most recent
+// function showDataInList(feature) {
+//     var programName = `<h3>${feature.properties['Program_Name']}</h3>`
+//     var cityElectionData = `<ul>
+//         <li>'Address: '+${feature.properties['Address']}</li>
+//         <li>${feature.properties['num_elections']+' election(s) within the last month'}</li>
+//         <li>${feature.properties['tot_wins']+' election(s) won'}</li>
+//         <li>${feature.properties['perc_win']+'% elections won'}</li>
+//         <li>${feature.properties['tot_challenged']+' challenged ballot(s)'}</li>
+//         <li>${feature.properties['tot_voted']+' total voter(s) in all recent elections'}</li>
+//         <li>${feature.properties['voter_turnout']+'% voter turnout'}</li>
+//     </ul>`;
+//     dataListEl.innerHTML = cityElectionData;
+//     dataTitleEl.innerHTML = programName;
+//   }
+
+
