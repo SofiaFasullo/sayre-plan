5c973a41f5b0ec737b7c0762896be37006dd3007
Sofia <sfasull1@binghamton.edu>
2023-12-11 07:23:28 -0500

    student data displays on command
    got a commend NOT linked to the checkboxes to display student data in stylized form

--- js/students.js
+++ js/students.js
@@ -16,15 +16,69 @@ function grabStudentOpportunitiesData(onSuccess, onFailure) {
     .then(onSuccess);
   }
 
-function ondataLoad(data) {
-    console.log(data);
+
+
+
+function showDataInList(feature) {
+  const spaceForData = document.querySelector('#space-for-data'); 
+  var programProfile = `
+            <div class="div">
+                <div class="content">
+                    <div class="program-name">
+                        <h1 class="text data-title">${feature.properties['Program_Name']}</h1>
+                        <h3 class="text data-subtitle">Need subtitle column in data</h3>
+                    </div>
+                    
+                    <div class="below-program-name">
+                        <div class="col col-left">
+                        <img class="data-image" src="./photos/post-secondary-dev/placeholder.jpg" alt="Image a leaf">
+                        <a class="text data-link" href="${feature.properties['Program_Link']}">External Site</a>
+                        <div class="sub-content specifics">
+                            <h2 class="text data-program-specifics">Program Specifics</h2>
+                            <h3 class="text data-specifics-content">${feature.properties['Other Considerations']}</h3>
+                        </div>
+                    </div>
+
+                    <div class="col col-right">
+                        <div class="sub-content about">
+                        <h2 class="text data-about">About</h2>
+                        <h3 class="text data-about-content">${feature.properties['About']}</h3>
+                    </div>
+
+                    <div class="sub-content provides">
+                        <h2 class="text data-program-provides">Program Provides</h2>
+                        <h3 class="text data-provides-content">${feature.properties['Program Provides']}</h3>
+                    </div>
+
+                    <div class="sub-content eligibility">
+                        <h2 class="text data-program-eligibility">Program Eligibility</h2>
+                        <h3 class="text data-eligibility-content">${feature.properties['Program_Eligibility']}</h3>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+`
+  spaceForData.innerHTML = programProfile;
+
 }
 
-grabStudentOpportunitiesData(ondataLoad);
+function testRun(programs) {
+  //console.log(programs.features);
+  for (const program of programs.features) {
+    const name = program.properties.Program_Name
+    if(name == "CHOP Career Path ") {
+      showDataInList(program);
+    }
+  }
 
+}
 
-const dataListEl = document.querySelector('#data-list'); 
-const dataTitleEl = document.querySelector('.election-info-title')
+function onDataLoad(data) {
+  testRun(data);
+}
+
+grabStudentOpportunitiesData(onDataLoad);
 
 //show most recent data as "last updated" and "earliest data from" most recent
 // function showDataInList(feature) {
